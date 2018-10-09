405d8d2f1c8a97537ac0057da48308a54350e818
Max <max.francis2000@gmail.com>
2018-10-09 11:28:28 +0000

    Added some tests and features to the program

--- lib/notebook.rb
+++ lib/notebook.rb
@@ -2,14 +2,24 @@ class Notebook
 
   def initialize
     @book = {}
+    @titles = []
   end
   
   def add(title, body)
+    @titles << title
     @book[:"#{title}"] = body
   end
   
+  def display
+    return @titles
+  end
+  
+  def display_text(display_title)
+    return @book[display_title]
+  end
+  
   def view
-    return @book
+    return @book 
   end
   
 end
--- spec/notebook_spec.rb
+++ spec/notebook_spec.rb
@@ -4,6 +4,35 @@ describe Notebook do
   
   describe '#add' do
     
+    it 'adds a title and body to the hash' do
+      notebook = Notebook.new
+      notebook.add('Title_test', 'Body_test')
+      expect(notebook.view).to include(:Title_test=>'Body_test')
+    end
+  end
+  
+  describe '#display' do
+    
+    it 'displays the different titles' do
+      
+      notebook = Notebook.new
+      notebook.add('Title_test_array', 'Body_test_array')
+      notebook.add('Title_test_array2', 'Body_test_array2')
+      expect(notebook.display).to include('Title_test_array')
+      expect(notebook.display).to include('Title_test_array2')
+      
+    end
+  end
+  
+  describe '#display_text' do
+    it 'displays the body of the title' do
+    
+      notebook = Notebook.new
+      notebook.add('Title_test', 'Body_test')
+      notebook.display_text('title_test')
+      expect(notebook.display_text('title_test')).to eq('Body_test')
+      
+    end
   end
   
 end
